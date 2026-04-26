97d43b175ce1e4cf29c38839511ee5cf43d53641
jmpxhr <bitbuk@gmail.com>
2026-04-26 19:58:55 +0300

    feat: add di

---
+++ server/common/di.py
@@ -0,0 +1,69 @@
+from collections.abc import Callable
+from typing import Any, final
+
+import punq
+
+
+def _global_namespace() -> dict[str, Any]:
+    from django.conf import LazySettings  # noqa: F401, PLC0415
+    from django.core.cache import BaseCache  # noqa: F401, PLC0415
+
+    return locals()
+
+
+def _create_injector[Thing](
+    container: punq.Container,
+    localns: dict[str, Any],
+) -> Callable[[Thing], Thing]:
+    # We need to provide the same string names as we do in the definition.
+    localns.pop('container')
+    localns.update(_global_namespace())
+    container.registrations._localns.update(localns)  # noqa: SLF001 # type: ignore
+    return lambda service: service
+
+
+def _inject_django(container: punq.Container) -> None:
+    from django.conf import LazySettings, settings  # noqa: PLC0415
+
+    # Django:
+    container.register(
+        LazySettings,
+        instance=settings,
+        scope=punq.Scope.singleton,
+    )
+
+
+def _inject_main(container: punq.Container) -> None:
+    # Hacks to resolve annotations:
+    inject = _create_injector(container, locals())
+
+    # Things to register:
+
+
+def _populate_dependencies(container: punq.Container) -> punq.Container:
+    """Populates dependencies for the container."""
+    # Deps:
+    _inject_django(container)
+    # Apps:
+    _inject_main(container)
+    return container
+
+
+class HasContainer:
+    """
+    Base class for all parts that use ``resolve()`` function.
+
+    Must be the first base class.
+    """
+
+    __slots__ = ('_container',)
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Create container with dependencies for this class."""
+        super().__init__(*args, **kwargs)
+        self._container = _populate_dependencies(punq.Container())
+
+    @final
+    def resolve[Thing](self, thing: type[Thing]) -> Thing:
+        """Resolve a dependency."""
+        return self._container.resolve(thing)  # type: ignore[no-any-return]
--- server/urls.py
+++ server/urls.py
@@ -20,14 +20,28 @@ from typing import TYPE_CHECKING
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.admindocs import urls as admindocs_urls
-from django.urls import include, path
+from django.urls import include
+from dmr.openapi import build_schema
+from dmr.openapi.views import SwaggerView
+from dmr.plugins.msgspec import MsgspecSerializer
+from dmr.routing import Router, build_404_handler, build_500_handler, path
 
 if TYPE_CHECKING:
     from django.urls import URLPattern, URLResolver
 
 admin.autodiscover()
 
+router = Router('api/', [])
+
+schema = build_schema(router)
+handler404 = build_404_handler(router.prefix, serializer=MsgspecSerializer)
+handler500 = build_500_handler(router.prefix, serializer=MsgspecSerializer)
+
 urlpatterns: list['URLPattern | URLResolver'] = [
+    # API:
+    path(router.prefix, include((router.urls, 'server'), namespace='api')),
+    # OpenAPI:
+    path('docs/', SwaggerView.as_view(schema), name='swagger'),
     # django-admin:
     path('admin/doc/', include(admindocs_urls)),
     path('admin/', admin.site.urls),
